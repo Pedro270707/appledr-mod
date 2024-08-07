db980eda7ce859de34f3dcb16a7e25428f0ec808
Pedro <pedro2007.prrr@gmail.com>
2024-08-07 11:52:45 -0300

    Added lore (should maybe be the lore component instead? idk)

--- src/main/java/net/pedroricardo/content/GreatHelmItem.java
+++ src/main/java/net/pedroricardo/content/GreatHelmItem.java
@@ -1,12 +1,11 @@
 package net.pedroricardo.content;
 
-import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import com.mojang.authlib.properties.PropertyMap;
 import eu.pb4.polymer.core.api.item.PolymerItem;
 import eu.pb4.polymer.core.api.item.PolymerItemUtils;
-import eu.pb4.polymer.core.api.utils.PolymerUtils;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.LoreComponent;
 import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
@@ -16,9 +15,12 @@ import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Optional;
 
 public class GreatHelmItem extends Item implements PolymerItem {
@@ -47,5 +49,10 @@ public class GreatHelmItem extends Item implements PolymerItem {
     public int getPolymerCustomModelData(ItemStack itemStack, @Nullable ServerPlayerEntity player) {
         Identifier id = Registries.ITEM.getId(this);
         return id.hashCode();
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable(this.getTranslationKey(stack) + ".desc").formatted(Formatting.GRAY));
     }
 }
