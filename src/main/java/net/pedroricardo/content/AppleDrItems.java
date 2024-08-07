271702e8bb0f2e5a8260001e3764d666cc21030d
Pedro <pedro2007.prrr@gmail.com>
2024-08-07 11:56:54 -0300

    Added techno greathelm

--- src/main/java/net/pedroricardo/content/AppleDrItems.java
+++ src/main/java/net/pedroricardo/content/AppleDrItems.java
@@ -12,6 +12,7 @@ import net.pedroricardo.AppleDrMod;
 public class AppleDrItems {
     public static final Item ROTTEN_APPLE = register("rotten_apple", new RottenAppleItem(new Item.Settings().food(FoodComponents.APPLE), Items.ROTTEN_FLESH));
     public static final Item APPLE_GREATHELM = register("apple_greathelm", new GreatHelmItem(new Item.Settings().rarity(Rarity.EPIC), "e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNmU2OTg4YWMzYzIzMGNhZDUzMDA4NjBlOTY1NjgxNWYyMzkwMDZkZmE3YzVmYzdhMmRkMjliODI2MzQzNWJiOSJ9fX0="));
+    public static final Item TECHNO_GREATHELM = register("techno_greathelm", new GreatHelmItem(new Item.Settings().rarity(Rarity.EPIC), "e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGNhNTM4Zjc4NzA0OGRiYTI3ZGNkYmJjYjcyZDJmNTc4Zjg1NzczMTY4ZDcyNDY2MjY2ZTc1NWY0NzFjODkifX19"));
 
     public static Item register(String id, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(AppleDrMod.MOD_ID, id), item);
