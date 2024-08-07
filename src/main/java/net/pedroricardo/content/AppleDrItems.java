9114efc557f0a1a114d21daaa684a3a8de4a03d6
Pedro <pedro2007.prrr@gmail.com>
2024-08-07 18:21:52 -0300

    Added item group

--- src/main/java/net/pedroricardo/content/AppleDrItems.java
+++ src/main/java/net/pedroricardo/content/AppleDrItems.java
@@ -1,10 +1,14 @@
 package net.pedroricardo.content;
 
+import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
 import net.minecraft.component.type.FoodComponents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.pedroricardo.AppleDrMod;
@@ -15,11 +19,19 @@ public class AppleDrItems {
     public static final Item TECHNO_GREATHELM = register("techno_greathelm", new GreatHelmItem(new Item.Settings().rarity(Rarity.EPIC), "e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGNhNTM4Zjc4NzA0OGRiYTI3ZGNkYmJjYjcyZDJmNTc4Zjg1NzczMTY4ZDcyNDY2MjY2ZTc1NWY0NzFjODkifX19"));
     public static final Item LEGACY_APPLE_GREATHELM = register("legacy_apple_greathelm", new GreatHelmItem(new Item.Settings().rarity(Rarity.EPIC), "e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTdjMWFlYzJhYTBkZTlhZGQ4NjM2MTM1NDRjZTYzNDBlYWQ2M2RkZmFiNGUzYmY0ZjEwYzgwMDZhY2Q2NzY3NSJ9fX0="));
 
+    public static void registerItemGroup(String id, ItemGroup group) {
+        PolymerItemGroupUtils.registerPolymerItemGroup(Identifier.of(AppleDrMod.MOD_ID, id), group);
+    }
+
     public static Item register(String id, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(AppleDrMod.MOD_ID, id), item);
     }
 
     public static void init() {
         AppleDrMod.LOGGER.debug("Registering items");
+
+        registerItemGroup("appledrmod", PolymerItemGroupUtils.builder().icon(() -> new ItemStack(Items.APPLE)).displayName(Text.translatable("itemGroup.appledrmod")).entries((ctx, entries) -> {
+            entries.add(ROTTEN_APPLE);
+        }).build());
     }
 }
